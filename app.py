cd622108d33e2e398984af18a9d618b9c83cee33
Rud356 <r.moroshkin2015@gmail.com>
2020-07-07 23:27:31 +0300

    Added auto-config creation

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import asyncio
+import configparser
 from os import mkdir
+from sys import exit
 
 from motor import motor_asyncio
 from quart import Quart
@@ -11,13 +13,54 @@ app.json_encoder = CustomJSONEncoder
 loop = asyncio.get_event_loop()
 connected_users = {}
 
-app.config["DEBUG"]: bool = True
-app.config["ALLOW_REG"]: bool = True
-app.config['UPLOAD_FOLDER']: str = "resourses/"
-app.config["DB_CONN_STR"]: str = "mongodb://localhost:27017"
-app.config["TTK"]: int = 10
-app.config['MAX_CONTENT_LENGTH']: int = 20 * 1024 * 1024 + 1
-app.config['LOGIN_CACHE_SIZE']: int = 1024
+
+def load_config():
+    config = configparser.ConfigParser(allow_no_value=True)
+    try:
+        with open("./settings.ini") as conf:
+            config.read_file(conf)
+        return config
+
+    except FileNotFoundError:
+        config.add_section("App")
+        config.set("App", "debug", "True")
+        config.set("App", "allow_reg", "True")
+        config.set("App", "upload_folder", "resourses/")
+        config.set("App", "# To see more about mongodb connection visit https://docs.mongodb.com/manual/reference/connection-string/", "")
+        config.set("App", "db_conn_string", "mongodb://localhost:27017")
+        config.set("App", "# TTK means the number of minutes until killing user from cache for inactive", "")
+        config.set("App", "TTK", "10")
+        config.set("App", "# Next parameter shows how big can be the uploaded file to server", "")
+        config.set("App", "max_payload_size", "20")
+        config.set("App", "login_cache_size", "1024")
+        config.set("App", "priv_key", "")
+        config.set("App", "pub_key", "")
+        config.set("App", "ca_certs", "")
+        config.set("App", "host", "localhost")
+        config.set("App", "port", "8080")
+
+        with open("settings.ini", 'w') as conf:
+            config.write(conf)
+
+        print("Change configuration file!")
+        exit(1)
+
+
+config = load_config()
+
+app.config["DEBUG"]: bool = config.getboolean("App", "debug") or False
+app.config["ALLOW_REG"]: bool = config.getboolean("App", "allow_reg") or False
+app.config['UPLOAD_FOLDER']: str = config.get("App", "upload_folder")
+app.config["DB_CONN_STR"]: str = config.get("App", "db_conn_string")
+app.config["TTK"]: int = config.getint("App", "TTK")
+app.config['MAX_CONTENT_LENGTH']: int = config.getfloat("App", "max_payload_size") * 1024 * 1024 + 1
+
+if config.getfloat("App", "max_payload_size") == float("inf"):
+    print("Do not set payload size as inf")
+    print("Resetted value to default limit: 20MB")
+    app.config['MAX_CONTENT_LENGTH'] = 20 * 1024 * 1024 + 1
+
+app.config['LOGIN_CACHE_SIZE']: int = config.getint("App", "login_cache_size")
 
 
 try:
--- start.py
+++ start.py
@@ -1,7 +1,8 @@
 from os import system, name
 
-from app import app, loop
+from app import app, loop, load_config
 from routes import init_routes
+
 
 if name == 'nt':
     system("cls")
@@ -9,9 +10,15 @@ if name == 'nt':
 else:
     system("clear")
 
+
+config = load_config()
+
 init_routes()
 app.run(
-    'localhost', 80,
+    config.get("App", 'host'), config.getint("App", 'port'),
     app.config['DEBUG'],
-    loop=loop
+    loop=loop,
+    ca_certs=config.get("App", "ca_certs") or None,
+    certfile=config.get("App", "pub_key") or None,
+    keyfile=config.get("App", "priv_key") or None
 )
