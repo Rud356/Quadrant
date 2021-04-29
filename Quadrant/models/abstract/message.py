da1b6a82ef13a7239342e593b172816f7ca0fab6
Rud356 <r.moroshkin2015@gmail.com>
2021-04-29 23:02:23 +0300

    Add docs for message.py and dm_channels.py

--- Quadrant/models/abstract/message.py
+++ Quadrant/models/abstract/message.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from datetime import datetime
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Optional
 from uuid import UUID
 
 from sqlalchemy import BigInteger, Boolean, Column, DateTime, ForeignKey, String, select
@@ -16,6 +16,9 @@ MESSAGES_PER_REQUEST = 100
 
 
 class ABCMessage(Base):
+    """
+    Represents abstract message class that we inherit to create messages tables for other types of chats.
+    """
     channel_id: Column
     message_id = Column(BigInteger, primary_key=True)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
@@ -24,19 +27,33 @@ class ABCMessage(Base):
     edited = Column(Boolean, default=False, nullable=False)
 
     text = Column(String(2000), nullable=True)
+    # TODO: add attached files table
     # attached_file_id = Column(ForeignKey('files.id'), nullable=True)
 
     __abstract__ = True
 
     @declared_attr
     def channel_id(self):
+        """
+        Foreign key on table with channel id.
+        """
         pass
 
     @declared_attr
     def author_id(self):
+        """
+        Foreign key on member of channel or server.
+        """
         pass
 
     async def user_can_send_message_check(self, author: users_package.User, *, session) -> None:
+        """
+        Checks if author can send a message to this channel.
+
+        :param author: instance of User that we check permissions for.
+        :param session: sqlalchemy session.
+        :return: nothing (raises exception if doesn't have permissions).
+        """
         pass
 
     @classmethod
@@ -44,6 +61,16 @@ class ABCMessage(Base):
         cls, channel, author: users_package.User, text: str, attached_file: Optional = None,
         *, session
     ) -> ABCMessage:
+        """
+        Creates new message in text channel.
+
+        :param channel: text channel instance.
+        :param author: authors User model instance.
+        :param text: message text that can be nothing (in case we have attached file) and not longer than 2000 symbols.
+        :param attached_file: attached file instance.
+        :param session: sqlalchemy session.
+        :return: new message instance if everything is correct.
+        """
 
         if not (await channel.is_member(channel.id, author, session=session)):
             raise cls.common_exc.UserIsNotAMemberException("You're not a member of chat")
@@ -68,6 +95,14 @@ class ABCMessage(Base):
 
     @classmethod
     async def get_message(cls, message_id: int, channel_id: UUID, *, session) -> ABCMessage:
+        """
+        Gives an message on channel with exact id.
+
+        :param message_id: message id we want to get from channel.
+        :param channel_id: channel id from which we request message.
+        :param session: sqlalchemy session.
+        :return: message (raises exception if didn't found any).
+        """
         query_result = await session.execute(
             select(cls).options(FromCache("messages")).filter(
                 cls.channel_id == channel_id, message_id == message_id
@@ -79,7 +114,15 @@ class ABCMessage(Base):
     @classmethod
     async def get_last_message(
         cls, channel_id: UUID, select_pinned_only: bool = False, *, session
-    ) -> ABCMessage:
+    ) -> Optional[ABCMessage]:
+        """
+        Gives latest message.
+
+        :param channel_id: channel id from which we request message.
+        :param select_pinned_only: flag that tells if we need to look up in pinned messages or not.
+        :param session: sqlalchemy session.
+        :return: message.
+        """
         query = select(cls).filter(
             cls.channel_id == channel_id
         ).order_by(cls.message_id.desc())
@@ -88,15 +131,24 @@ class ABCMessage(Base):
             query = query.filter(cls.pinned.is_(True))
 
         query_result = await session.execute(query)
-        return await query_result.one()
+        return await query_result.one_or_none()
 
     @classmethod
     async def get_messages_before(
         cls, message_id: int, channel_id: UUID, select_pinned_only: bool = False, *, session
     ) -> Tuple[ABCMessage]:
+        """
+        Gives messages that came before specified messages on timeline. This doesn't includes a message we look from.
+
+        :param message_id: message id we want to start looking from.
+        :param channel_id: channel id from which we request messages.
+        :param select_pinned_only: flag that tells if we need to look up in pinned messages or not.
+        :param session: sqlalchemy session.
+        :return: messages that came before specified one.
+        """
         query = select(cls).filter(
             cls.channel_id == channel_id, cls.message_id < message_id
-        ).limit(MESSAGES_PER_REQUEST)
+        ).limit(MESSAGES_PER_REQUEST).order_by(cls.message_id.desc())
 
         if select_pinned_only:
             query = query.filter(cls.pinned.is_(True))
@@ -108,9 +160,18 @@ class ABCMessage(Base):
     async def get_messages_after(
         cls, message_id: int, channel_id: UUID, select_pinned_only: bool = False, *, session
     ) -> Tuple[ABCMessage]:
+        """
+        Gives messages that came after specified messages on timeline. This doesn't includes a message we look from.
+
+        :param message_id: message id we want to start looking from.
+        :param channel_id: channel id from which we request messages.
+        :param select_pinned_only: flag that tells if we need to look up in pinned messages or not.
+        :param session: sqlalchemy session.
+        :return: messages that came after specified one.
+        """
         query = select(cls).filter(
             cls.channel_id == channel_id, cls.message_id > message_id
-        ).limit(MESSAGES_PER_REQUEST)
+        ).limit(MESSAGES_PER_REQUEST).order_by(cls.message_id.desc())
 
         if select_pinned_only:
             query = query.filter(cls.pinned.is_(True))
@@ -119,6 +180,13 @@ class ABCMessage(Base):
         return await query_result.all()
 
     async def delete_message_by_author(self, delete_by: users_package.User, *, session) -> None:
+        """
+        Deletes message that been sent by exact user.
+
+        :param delete_by: the user that asked to delete message.
+        :param session: sqlalchemy session.
+        :return: nothing (raises exceptions).
+        """
         if self.author_id != delete_by.id:
             raise PermissionError("User tries to edit message even if he's not an author")
 
@@ -126,6 +194,14 @@ class ABCMessage(Base):
         await session.commit()
 
     async def edit_message(self, new_text: str, edit_by: users_package.User, *, session) -> None:
+        """
+        Updates messages text (this can be executed only by author of message).
+
+        :param new_text: text that will be set.
+        :param edit_by: User instance that trying to edit message.
+        :param session: sqlalchemy session.
+        :return: nothing (raises exceptions).
+        """
         if self.author_id != edit_by.id:
             raise PermissionError("User tries to edit message even if he's not an author")
 
@@ -138,6 +214,12 @@ class ABCMessage(Base):
         await session.commit()
 
     async def pin_message(self, *, session) -> None:
+        """
+        Pins message.
+
+        :param session: sqlalchemy session.
+        :return: nothing.
+        """
         # Warning: permission check required
         if self.pinned:
             raise ValueError("Already pinned")
@@ -146,6 +228,12 @@ class ABCMessage(Base):
         await session.commit()
 
     async def unpin_message(self, *, session) -> None:
+        """
+        Unpins message.
+
+        :param session: sqlalchemy session.
+        :return: nothing.
+        """
         # Warning: permission check required
         if not self.pinned:
             raise ValueError("Message isn't pinned")
@@ -155,4 +243,7 @@ class ABCMessage(Base):
 
     class common_exc:
         class UserIsNotAMemberException(PermissionError):
+            """
+            Exception that represents that user isn't a channel member anymore.
+            """
             pass
