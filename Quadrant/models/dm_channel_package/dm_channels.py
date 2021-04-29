a50b32c56d344f75c5dfffac3de91f48402a3aeb
Rud356 <r.moroshkin2015@gmail.com>
2021-04-29 23:02:23 +0300

    Add docs for message.py and dm_channels.py

--- Quadrant/models/dm_channel_package/dm_channels.py
+++ Quadrant/models/dm_channel_package/dm_channels.py
@@ -28,11 +28,27 @@ class DirectMessagesChannel(Base):
         )
 
     async def other_participant(self, requester_user: users_package.User) -> DMParticipant:
+        """
+        Gives other participant instance.
+
+        :param requester_user: user that asks for instance of other participant.
+        :return: DMParticipant instance.
+        """
         participant = await self.participants.filter(DMParticipant.user_id != requester_user.id).one()
-        return participant.user
+        return participant
 
     @classmethod
     async def create_channel(cls, initiator: users_package.User, with_user: users_package.User, *, session):
+        """
+        Initializes new dm channel.
+
+        :param initiator: user who initiates dm channel.
+        :param with_user: user instance with whom we create dm channel.
+        :param session: sqlalchemy session.
+        :return: new dm channel instance.
+        """
+        # TODO: add setting to limit initiation of dms channels per server or based on something else.
+
         new_channel = DirectMessagesChannel(
             participants=[
                 DMParticipant(user_id=initiator.id),
@@ -44,9 +60,18 @@ class DirectMessagesChannel(Base):
         return new_channel
 
     @classmethod
-    async def get_channel_by_id(cls, channel_id: UUID, *, session):
+    async def get_channel_by_id(cls, channel_id: UUID, requester: users_package.User, *, session):
+        """
+        Gives exact dm channel
+        :param channel_id: channel id.
+        :param requester: user instance of someone who asks for channel.
+        :param session: sqlalchemy session.
+        :return:
+        """
         try:
-            return await session.options(FromCache("dm_channels")).get(cls, channel_id)
+            return await session.options(FromCache("dm_channels")).filter(
+                cls.participants.id.is_(requester.id)
+            ).get(cls, channel_id)
 
         except (NoResultFound, OverflowError):
             raise ValueError("No such dm channel with specified id")
@@ -59,9 +84,17 @@ class DirectMessagesChannel(Base):
         return await query_result.one()
 
     @classmethod
-    async def create_or_get_channel_by_participants(
+    async def get_or_create_channel_by_participants(
         cls, requester: users_package.User, with_user: users_package.User, *, session
     ):
+        """
+        Gets channel by participants and in case it wasn't found - creates new one.
+
+        :param requester: user instance of someone who asks for channel.
+        :param with_user: user instance with whom we want to get channel in too.
+        :param session: sqlalchemy session.
+        :return: dm channel.
+        """
         try:
             return await cls.get_channel_by_participants(requester, with_user, session=session)
 
@@ -73,6 +106,15 @@ class DirectMessagesChannel(Base):
     async def participants_have_channel(
         cls: DirectMessagesChannel, requester: users_package.User, with_user: users_package.User, *, session
     ) -> bool:
+        """
+        Checks if participants have channel.
+
+        :param cls: DirectMessagesChannel class.
+        :param requester: user instance of someone who asks for channel.
+        :param with_user: user instance with whom we want to get channel in too.
+        :param session: sqlalchemy session.
+        :return: dm channel.
+        """
         query_result = await session.execute(
             cls._channel_by_participants(cls, requester, with_user).exists()
         ).scalar() or False
@@ -81,6 +123,14 @@ class DirectMessagesChannel(Base):
 
     @staticmethod
     def _channel_by_participants(cls, requester: users_package.User, with_user: users_package.User):
+        """
+        Gives query that helps finding channels where both participants are in.
+
+        :param cls: DirectMessagesChannel class.
+        :param requester: user instance of someone who asks for channel.
+        :param with_user: user instance with whom we want to get channel in too.
+        :return: sqlalchemy query.
+        """
         return select(cls).filter(
             cls.participants.all_(
                 DMParticipant.user_id.in_((requester.id, with_user.id))
@@ -89,6 +139,14 @@ class DirectMessagesChannel(Base):
 
     @staticmethod
     async def is_member(channel_id: UUID, user: users_package.User, *, session) -> bool:
+        """
+        Checks if user is member of this channel.
+
+        :param channel_id: channel id.
+        :param user: user instance of someone who asks for channel.
+        :param session: sqlalchemy session.
+        :return: bool value representing if user is a member.
+        """
         exists_query = select(DMParticipant).filter(
             DMParticipant.user_id == user.id,
             DMParticipant.channel_id == channel_id
